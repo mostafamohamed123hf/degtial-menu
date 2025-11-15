b22115b507b6427fe7690416dc5e1f8ff26718d6
mostafamohamed123hf <115039919+mostafamohamed123hf@users.noreply.github.com>
2025-11-15 23:37:37 +0200

    Update csrf.js
    Added 'https://degtial-menu.vercel.app' to allowed origins.

--- backend/middleware/csrf.js
+++ backend/middleware/csrf.js
@@ -18,6 +18,7 @@ const isOriginAllowed = (origin) => {
         "http://localhost:5500",
         "http://localhost:5000",
         "http://127.0.0.1:5000",
+        "https://degtial-menu.vercel.app",
       ];
 
   return allowedOrigins.some((allowed) => origin.startsWith(allowed));
