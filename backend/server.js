6e10930f69107cc9acb3e03845970573813085d4
mostafamohamed123hf <115039919+mostafamohamed123hf@users.noreply.github.com>
2025-11-16 13:33:41 +0200

    Add routes for service worker and manifest files

--- backend/server.js
+++ backend/server.js
@@ -361,6 +361,14 @@ app.use('/assets', express.static(path.join(__dirname, "../assets")));
 app.use('/admin', express.static(path.join(__dirname, "../admin")));
 app.use('/sounds', express.static(path.join(__dirname, "../sounds")));
 
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.join(__dirname, "../service-worker.js"));
+});
+
+app.get('/manifest.json', (req, res) => {
+  res.sendFile(path.join(__dirname, "../manifest.json"));
+});
+
 // Add a specific route for /register to redirect to register.html
 app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/pages/register.html"));
