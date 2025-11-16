ce3a0efadd5fddf105f1f799402e60b742bdd9de
mostafamohamed123hf <115039919+mostafamohamed123hf@users.noreply.github.com>
2025-11-16 02:54:32 +0200

    Serve static files from the admin directory

--- backend/server.js
+++ backend/server.js
@@ -357,6 +357,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 // Explicitly serve assets folder for uploaded images
 app.use('/assets', express.static(path.join(__dirname, "../assets")));
+app.use('/admin', express.static(path.join(__dirname, "../admin")));
 
 // Add a specific route for /register to redirect to register.html
 app.get("/register", (req, res) => {
